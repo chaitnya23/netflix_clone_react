9e6fca3839f4b713367028272b7a64a56f19bb9e
chaitnya23 <chaitnyagiri2306@gmail.com>
2022-02-15 04:53:26 +0530

    netflix clone react js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./bootstrap.css";
+import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import requests from "./requests";
+import Banner from "./components/Banner";
+import MovieRow from "./components/MovieRow";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Banner featchUrl={requests.netflixOriginals} />
+      <MovieRow
+        title="Netflix Originals"
+        featchUrl={requests.netflixOriginals}
+        islargeRow
+      />
+      <MovieRow title="Action" featchUrl={requests.getActionMovies} />
+      <MovieRow title="Horror" featchUrl={requests.getHorrorMovies} />
+      <MovieRow title="Comedy" featchUrl={requests.getComedyMovies} />
+      <MovieRow title="Romance" featchUrl={requests.getRomanceMovies} />
+
+    </>
   );
 }
 
